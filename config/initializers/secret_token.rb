9ccbb31b7c4fa4000f309985d96430c5bf27fa8a
Anatoly Pedemonte Ku <apedemonte@gmail.com>
2011-02-23 11:40:15 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RsyDemoApp::Application.config.secret_token = '9a836be4e3f2e18dcb6a4e3b7f0ada7929fd3330d4b443584fdf30e705b2fdb7392fa2eacad6de0080116b86d0da6bbb9dae3ac6510573457996eaaa0d8686bf'
