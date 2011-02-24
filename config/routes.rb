993ea56982d1aaff4bc9d82016c3ee82efe29a66
Anatoly Pedemonte Ku <apedemonte@gmail.com>
2011-02-24 01:02:54 -0500

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 RsyDemoApp::Application.routes.draw do
+
+  get "users/new"
+
   get "pages/home"
 
   get "pages/contact"
 
   get "pages/about"
   
-
+ 
+  
+match '/signup', :to => 'users#new'
 match '/contact', :to => 'pages#contact'
 match '/about', :to => 'pages#about'
 match '/help', :to => 'pages#help'
